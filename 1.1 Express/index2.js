1e6a6412d4994d3c21dca3e96f594b30ed7fdcac
Nirmalfageria <nirmal_b221082ee@nitc.ac.in>
2024-10-03 19:12:55 +0530

    middleware tutorial

--- 1.1 Express/index2.js
+++ 1.1 Express/index2.js
@@ -1,1 +1,47 @@
-// it is for the morgan middleware
+import morgan from "morgan";
+import express from "express";
+import { dirname } from "path";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import path from "path"; // Import path module
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+let isAuthorized = false; // Corrected spelling
+
+// Middleware to check password
+function passCheck(req, res, next) {
+    const { password } = req.body;
+    if (password === "123") {
+        isAuthorized = true;
+    } else {
+        isAuthorized = false; // Reset authorization on wrong password
+    }
+    next();
+}
+
+app.use(passCheck);
+
+// Serve the main page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Handle form submission
+app.post("/submit", (req, res) => {
+    if (isAuthorized) {
+        res.sendFile(path.join(__dirname, "public", "secret.html"));
+    } else {
+        res.send("<script>alert('You are not authorized to access this page'); window.location.href='/';</script>");
+    }
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
