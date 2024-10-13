2f433351d19dc47cb06d88243d5669f6e90f3667
Nirmalfageria <nirmal_b221082ee@nitc.ac.in>
2024-10-13 18:58:03 +0530

    Token based auth

--- 1.1 Express/index4.js
+++ 1.1 Express/index4.js
@@ -67,28 +67,62 @@ app.set('view engine', 'ejs');
 // );
 
 // ApiKey authentication
-app.get('/', (req, res) => {
-  res.render("index4.ejs");
-  res.send("This is the home page");
-});
+  // app.get('/', (req, res) => {
+  //   res.render("index4.ejs");
+  //   res.send("This is the home page");
+  // });
 
-app.post('/', async (req, res) => {
+  // app.post('/', async (req, res) => {
+  //   try {
+  //     let apiKeyResponse = await axios.get("https://secrets-api.appbrewery.com/generate-api-key");
+  //     let apiKey = apiKeyResponse.data.apiKey;
+
+  //     let dataResponse = await axios.get(`https://secrets-api.appbrewery.com/filter?score=5&apiKey=${apiKey}`);
+
+  //     const result = dataResponse.data;
+  //     res.render("solution4.ejs", { data: result, success: "Data fetched successfully!" });
+
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error.message);
+  //     res.render("solution4.ejs", { error: "An error occurred while fetching data. Please try again later." });
+  //   }
+  // });
+
+  // // token authentication
+ app.get('/', (req, res) => {
+res.render("index4.ejs");
+ })
+
+ app.post('/', async (req, res) => {
+  let { username, password } = req.body;
   try {
-    let apiKeyResponse = await axios.get("https://secrets-api.appbrewery.com/generate-api-key");
-    let apiKey = apiKeyResponse.data.apiKey;
+    let response = await axios.post("https://secrets-api.appbrewery.com/get-auth-token", {
+      username: username,
+      password: password
+    });
 
-    let dataResponse = await axios.get(`https://secrets-api.appbrewery.com/filter?score=5&apiKey=${apiKey}`);
+    let token = response.data.token;
+// let  token = "8e5a9d02-a3df-42cd-9742-32230abd21a0";
+console.log(token);
+    let dataResponse = await axios.get("https://secrets-api.appbrewery.com/user-secrets", {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
 
     const result = dataResponse.data;
-    res.render("solution4.ejs", { data: result, success: "Data fetched successfully!" });
-
+    // console.log(result);
+    if (Array.isArray(result)) {
+      res.render("solution4.ejs", { data: result, success: "Data fetched successfully!" });
+    } else {
+      // If it's not an array, pass an empty array or handle the error
+      res.render("solution4.ejs", { data: [], success: "Data fetched successfully!" });
+    }
   } catch (error) {
     console.error("Error fetching data:", error.message);
     res.render("solution4.ejs", { error: "An error occurred while fetching data. Please try again later." });
   }
-});
-
-
+ })
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
